821ddc01237ddd97dab359b37ff8752ba62c736c
xielingxiao <274105278@qq.com>
2018-06-08 08:35:14 +0800

    'update'

--- routes/route.js
+++ routes/route.js
@@ -4,6 +4,7 @@ var router = express.Router();
 var index = require('../data/index.json');
 var service = require('../data/service');
 var item = require('../data/item')
+var comments = require('../data/comment')
 
 router.get('/msite',function (req,res) {
   res.send(index)
@@ -14,5 +15,10 @@ router.get('/service',function (req,res) {
 router.get('/item',function (req,res) {
   res.send(item)
 })
-//暴露
+router.get('/comment',function (req,res) {
+  var page = req.query.page/1;
+  var comment = comments.slice(page*10,10*(page+1))
+  res.send(comment)
+})
+
 module.exports = router;
